25c615a030cd36fc470ff797a358da971d50d6ad
Kevyn <Kevyn.Pair@gmail.com>
2015-04-10 15:14:52 -0400

    example 1 with extra prints

---
+++ exercises/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("Hello world1.");
+    puts("Hello world2.");
+    puts("Hello world3.");
+    puts("Hello world4.");
+
+    return 0;
+}
